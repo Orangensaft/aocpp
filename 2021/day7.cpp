912527716c7c6fdab353b3a9eb271d2e84b517b2
Nicolas Meisberger <nicolas.meisberger@fintiba.com>
2021-12-07 09:10:00 +0100

    Cleanup

--- 2021/day7.cpp
+++ 2021/day7.cpp
@@ -38,34 +38,33 @@ int get_fuel_cost_v2(vector<int> *positions, int target){
     return out;
 }
 
-// brute force the shit out of it
-void y2021::day7::solve_part_1() {
+int find_optimal_fuel_cost(bool part2=false){
     auto positions = get_crabs();
     int start_pos = *min_element(positions.begin(), positions.end());
     int end_pos = *max_element(positions.begin(), positions.end());
     int cost;
     int found_cost=INT_MAX;
     for (int pos=start_pos; pos<=end_pos; pos++){
-        cost = get_fuel_cost(&positions, pos);
+        if (part2){
+            cost = get_fuel_cost_v2(&positions, pos);
+        } else {
+            cost = get_fuel_cost(&positions, pos);
+        }
         if (cost < found_cost) {
             found_cost = cost;
         }
     }
+    return found_cost;
+}
 
+
+// brute force the shit out of it
+void y2021::day7::solve_part_1() {
+    int found_cost = find_optimal_fuel_cost(false);
     cout << "Day 2021.7.1: " << found_cost << endl;
 }
 
 void y2021::day7::solve_part_2() {
-    auto positions = get_crabs();
-    int start_pos = *min_element(positions.begin(), positions.end());
-    int end_pos = *max_element(positions.begin(), positions.end());
-    int cost;
-    int found_cost=INT_MAX;
-    for (int pos=start_pos; pos<=end_pos; pos++){
-        cost = get_fuel_cost_v2(&positions, pos);
-        if (cost < found_cost) {
-            found_cost = cost;
-        }
-    }
+    int found_cost = find_optimal_fuel_cost(true);
     cout << "Day 2021.7.2: " << found_cost << endl;
 }
